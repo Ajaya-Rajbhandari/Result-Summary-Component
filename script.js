b344c0043668ce91179efe6508a2512e602da358
Ajaya-Rajbhandari <ajaya.shrestha.521@gmail.com>
2024-09-13 21:19:41 +0545

    try to do differently

---
+++ script.js
@@ -0,0 +1,7 @@
+// Load the JSON Data
+fetch("data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const scoreElement = document.getElementById("score");
+    const maxScore = document.getElementById("");
+  });
