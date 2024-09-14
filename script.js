d4c41668a2e7af37f1620439da5d4d2af5f7d207
Ajaya-Rajbhandari <ajaya.shrestha.521@gmail.com>
2024-09-14 22:45:03 +0545

    css changes

--- script.js
+++ script.js
@@ -2,6 +2,22 @@
 fetch("data.json")
   .then((response) => response.json())
   .then((data) => {
-    const scoreElement = document.getElementById("score");
-    const maxScore = document.getElementById("");
-  });
+    // const category = document.getElementById("category");
+    // const score = document.getElementById("score");
+    // const icon = document.getElementById("icon");
+
+    const detailedSummary = document.getElementById("detail-summery-items");
+    detailedSummary.innerHTML = "";
+    data.forEach((item) => {
+      const itemElement = document.createElement("div");
+      itemElement.innerHTML = `
+      <div class="summary-${item.category}">
+      <img src="${item.icon}" alt="${item.Category} icon "</img>
+      <span> ${item.category}</span>
+      <span> ${item.score}/100</span>
+      </div>
+      `;
+      detailedSummary.appendChild(itemElement);
+    });
+  })
+  .catch((error) => console.error("Error loading data:", error));
